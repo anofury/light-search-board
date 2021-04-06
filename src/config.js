1fda181ea2b44eee94247969f5409203b26074d8
xiaozunfang <41274736+anofury@users.noreply.github.com>
2021-04-06 10:15:09 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-export const SEARCH_API = 'https://sec.anquanxiaozhan.com/search/'
-export const APPLY_API = 'https://sec.anquanxiaozhan.com/apply/'
+export const SEARCH_API = 'https://XXXX.XXXX.XXXX/search/'
+export const APPLY_API = 'https://XXXX.XXXX.XXXX/apply/'
 
 // 搜索模式
 export const SEARCH_MODE = [
